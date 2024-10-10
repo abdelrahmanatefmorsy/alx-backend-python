5944c10bd03776c0b1d3d9f1b06ae68eaba4e390
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-10-10 18:03:16 +0300

    add method

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,4 +4,4 @@
 
 def add(a: float, b: float) -> float:
     """ Function """
-    return a + b  
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Moudule """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Function """
+    return str1 + str2
