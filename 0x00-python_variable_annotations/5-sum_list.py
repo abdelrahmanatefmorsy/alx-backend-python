ce3c988f04235e7986e6cb9226755e3f990fda42
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-10-10 18:25:10 +0300

    hello

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,7 @@ from typing import Callable, Iterator, Union, Optional, List
 def sum_list(input_list: List[float]) -> float:
     """ Function """
     sum = 0
+
     for i in input_list:
-        sum+=i
+        sum += i
     return sum
