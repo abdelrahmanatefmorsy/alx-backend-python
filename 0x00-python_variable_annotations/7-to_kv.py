27a187bc4309b0a3b0fbd1d0ba182713df31206f
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-10-10 18:34:03 +0300

    task 6 , 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Moudule """
-from typing import Union, List
+from typing import Union, List, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> tuple:
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """ Function """
     return (k, v ** 2)
