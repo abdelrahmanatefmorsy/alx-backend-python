dcc4edf52c67b8feae52a21a896195f130549d53
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-10-10 18:19:57 +0300

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Moudule """
+import math
+
+
+def sum_list(input_list : list[float]) -> float:
+    """ Function """
+    sum = 0
+    for i in input_list:
+        sum+=i
+    return sum
